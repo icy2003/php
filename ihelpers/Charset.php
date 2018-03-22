6ac09cfbaaf9194e6b3cf3a1feea0a977a6f5c48
icy2003 <2317216477@qq.com>
2018-03-22 19:39:06 +0800

    1、编码处理类 Charset.php 2、计时器 Timer.php

---
+++ ihelpers/Charset.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace icy2003\ihelpers;
+
+/**
+ * 编码转换.
+ */
+class Charset
+{
+    /**
+     * 检测字符串的编码格式.
+     *
+     * @param string $string
+     *
+     * @return string 编码
+     */
+    public static function detect($string)
+    {
+        $charset = mb_detect_encoding($string, 'UTF-8, ISO-8859-2, ASCII, UTF-7, EUC-JP, SJIS, eucJP-win, SJIS-win, JIS, ISO-2022-JP');
+        // `mb_detect_encoding` 会把 `WINDOWS-1250` 处理成 `ISO-8859-2`
+        if ('ISO-8859-2' === $charset && preg_match('~[\x7F-\x9F\xBC]~', $string)) {
+            $charset = 'WINDOWS-1250';
+        }
+
+        return $charset;
+    }
+
+    /**
+     * 将任意编码的字符串转成 UTF-8.
+     *
+     * @param string $string
+     *
+     * @return string 转码后的字符串
+     */
+    public static function convert2utf($string, $charset = '')
+    {
+        $charset = $charset ?: static::detect($string);
+        $converted = @iconv($charset, 'UTF-8//TRANSLIT//IGNORE', $string);
+        if (false === $converted) {
+            return '';
+        }
+
+        return $converted;
+    }
+}
---
+++ ihelpers/Timer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace icy2003\ihelpers;
+
+/**
+ * 计时器
+ *
+ * @filename Timer.php
+ * @encoding UTF-8
+ *
+ * @author icy2003 <2317216477@qq.com>
+ */
+class Timer
+{
+    private static $timers = [];
+
+    public static function start($name = '')
+    {
+        $start = microtime(true);
+        if (empty($name)) {
+            $name = md5($start.rand(0, 100));
+        }
+        self::$timers[$name] = $start;
+
+        return $name;
+    }
+
+    public static function end($name)
+    {
+        $delta = 0.0;
+        if (isset(self::$timers[$name])) {
+            $delta = microtime(true) - self::$timers[$name];
+            unset(self::$timers[$name]);
+        }
+
+        return $delta;
+    }
+
+    public static function timer()
+    {
+        static $time = 0;
+        $previousTime = $time;
+        $time = microtime(true);
+
+        return (0 === $previousTime) ? 0.0 : ($time - $previousTime);
+    }
+}
