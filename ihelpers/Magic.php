5c7774bc25a4aaefb36bcd156efb947f889a1bb7
icy2003 <2317216477@qq.com>
2018-09-23 19:18:18 +0800

    一个神奇的类

---
+++ ihelpers/Magic.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace icy2003\ihelpers;
+
+class Magic
+{
+    /**
+     * 消除尾递归.
+     *
+     * @param callable $callback 使用匿名函数尾调用的函数
+     * @param array    $params   函数参数
+     *
+     * @example
+     *
+     * 以斐波那契函数为例子
+     * function factorial($n, $accumulator = 1)
+     * {
+     *     if (0 == $n) {
+     *        return $accumulator;
+     *     }
+     *     return function () use ($n, $accumulator) {
+     *        return factorial($n - 1, $accumulator * $n);
+     *     };
+     * }
+     * Magic::tailRecursion('factorial', array(100));
+     *
+     * @return mixed
+     */
+    public static function tailRecursion($callback, $params)
+    {
+        $result = call_user_func_array($callback, $params);
+        while (is_callable($result)) {
+            $result = $result();
+        }
+
+        return $result;
+    }
+}
