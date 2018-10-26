69eca6597f8f5dbed859c6317d2a9030b46a7082
icy2003 <2317216477@qq.com>
2018-10-26 15:43:37 +0800

    [+]增加别名的访问方式

--- BaseI.php
+++ BaseI.php
@@ -2,8 +2,11 @@
 
 namespace icy2003;
 
+use icy2003\ihelpers\Arrays;
+
+
 defined('I_DEBUG') || define('I_DEBUG', false);
-$defaultConfig = __DIR__ . '/config.php';
+$defaultConfig = '@icy2003/config.php';
 defined("I_DEFAULT_CONFIG_FILE") || define("I_DEFAULT_CONFIG_FILE", $defaultConfig);
 defined("I_CONFIG_FILE") || define("I_CONFIG_FILE", $defaultConfig);
 
@@ -17,10 +20,14 @@ defined("I_CONFIG_FILE") || define("I_CONFIG_FILE", $defaultConfig);
  */
 class BaseI
 {
+    public static $aliases = [
+        '@icy2003' => __DIR__,
+    ];
     public static function autoload($className)
     {
+
         if (false !== strpos($className, '\\')) {
-            $classFile = dirname(__DIR__) . '/' . str_replace('icy2003', 'icy2003_php', str_replace('\\', '/', $className) . '.php');
+            $classFile = static::getAlias('@' . str_replace('\\', '/', $className) . '.php', false);
             if (false === $classFile || !is_file($classFile)) {
                 return;
             }
@@ -36,6 +43,72 @@ class BaseI
     public static function getVersion()
     {
         return '0.0.1';
+    }
+
+    public static function getAlias($alias)
+    {
+        if (strncmp($alias, '@', 1)) {
+            return $alias;
+        }
+
+        $pos = strpos($alias, '/');
+        $root = $pos === false ? $alias : substr($alias, 0, $pos);
+
+        if (isset(static::$aliases[$root])) {
+            if (is_string(static::$aliases[$root])) {
+                return $pos === false ? static::$aliases[$root] : static::$aliases[$root] . substr($alias, $pos);
+            }
+
+            foreach (static::$aliases[$root] as $name => $path) {
+                if (strpos($alias . '/', $name . '/') === 0) {
+                    return $path . substr($alias, strlen($name));
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static function setAlias($alias, $path)
+    {
+        if (strncmp($alias, '@', 1)) {
+            $alias = '@' . $alias;
+        }
+        $pos = strpos($alias, '/');
+        $root = $pos === false ? $alias : substr($alias, 0, $pos);
+        if ($path !== null) {
+            $path = strncmp($path, '@', 1) ? rtrim($path, '\\/') : static::getAlias($path);
+            if (!isset(static::$aliases[$root])) {
+                if ($pos === false) {
+                    static::$aliases[$root] = $path;
+                } else {
+                    static::$aliases[$root] = [$alias => $path];
+                }
+            } elseif (is_string(static::$aliases[$root])) {
+                if ($pos === false) {
+                    static::$aliases[$root] = $path;
+                } else {
+                    static::$aliases[$root] = [
+                        $alias => $path,
+                        $root => static::$aliases[$root],
+                    ];
+                }
+            } else {
+                static::$aliases[$root][$alias] = $path;
+                krsort(static::$aliases[$root]);
+            }
+        } elseif (isset(static::$aliases[$root])) {
+            if (is_array(static::$aliases[$root])) {
+                unset(static::$aliases[$root][$alias]);
+            } elseif ($pos === false) {
+                unset(static::$aliases[$root]);
+            }
+        }
+    }
+
+    public static function config($name)
+    {
+        return Arrays::value(Arrays::arrayMergeRecursive(require static::getAlias(I_DEFAULT_CONFIG_FILE), require static::getAlias(I_CONFIG_FILE)), $name);
     }
 
     /**
