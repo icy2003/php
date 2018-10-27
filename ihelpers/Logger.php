f04b5999e737adcfa4440283c72ad6bcd2a449e6
icy2003 <2317216477@qq.com>
2018-10-27 22:33:16 +0800

    [+]添加 info 函数 [*]整理 errorHandler，支持更多配置

--- ihelpers/Logger.php
+++ ihelpers/Logger.php
@@ -36,11 +36,76 @@ class Logger
     }
     public function errorHandler($errno, $errstr, $errfile, $errline)
     {
-        if ($this->config['useFile']) {
-            $logPath = BaseI::getAlias(trim($this->config['filePath'], '/') . '/');
-            $logString = date('Y-m-d H:i:s') . " [{$errfile}]({$errno})第 {$errline} 行：{$errstr}" . PHP_EOL;
+        $config = $this->config;
+        $typeArray = explode(',', $config['type']);
+        $map = [
+            '{date}' => date($config['dateFormat']),
+            '{errfile}' => $errfile,
+            '{errno}' => $errno,
+            '{errline}' => $errline,
+            '{errstr}' => $errstr,
+        ];
+        $string = str_replace(array_keys($map), array_values($map), $config['errorTemplete']);
+        static::handler($config, $string);
+    }
+    /**
+     * 让 echo 可以当函数使用
+     *
+     * @param string $string
+     * @return void
+     */
+    public static function echo($string)
+    {
+        echo $string;
+    }
+
+    /**
+     * 返回 var_dump 的值的字符串
+     *
+     * @param mixed $data
+     * @return string
+     */
+    public static function varDump($data)
+    {
+        ob_start();
+        var_dump($data);
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+
+    public static function info($message)
+    {
+        $config = BaseI::config('Logger');
+        if (false === $config['isLog']) {
+            return;
+        }
+        $array = debug_backtrace();
+        $map = [
+            '{date}' => date($config['dateFormat']),
+            '{file}' => $array[0]['file'],
+            '{line}' => $array[0]['line'],
+            '{message}' => $message,
+        ];
+        $string = str_replace(array_keys($map), array_values($map), $config['infoTemplete']);
+        static::handler($config, $string);
+    }
+
+    private static function handler($config, $string)
+    {
+        $typeArray = explode(',', $config['type']);
+        if (in_array('file', $typeArray)) {
+            $logPath = BaseI::getAlias(trim($config['file']['filePath'], '/') . '/');
             FileIO::createDir($logPath);
-            file_put_contents($logPath . date('Y-m-d') . '.log', $logString, FILE_APPEND);
+            $fileName = $config['file']['fileName'];
+            if (is_callable($fileName)) {
+                $fileName = $fileName();
+            }
+            file_put_contents($logPath . $fileName, $string . PHP_EOL, $config['file']['flag']);
+        }
+        if (in_array('print', $typeArray)) {
+            $function = $config['print']['function'];
+            is_callable($function) && call_user_func($function, $string . PHP_EOL);
         }
     }
 }
