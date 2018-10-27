cdcde435977d3a75e43d6240a0bb6eca9b35aaf4
icy2003 <2317216477@qq.com>
2018-10-27 23:11:41 +0800

    [*]调整配置

--- config.php
+++ config.php
@@ -3,19 +3,19 @@
 return [
     'Logger' => [
         'isLog' => true,
-        'infoTemplete' => '{date} [INFO] {file}(line:{line}) {message}',// 2018-10-27 19:01:21 [INFO] test.php(line:8) hello world
-        'errorTemplete' => '{date} [ERROR:{errno}] {errfile}(line:{errline}) {errstr}',// 2018-10-27 19:01:21 [ERROR:8] test.php(line:4) Undefined variable: var
+        'infoTemplete' => '{date} [INFO] {message} {file}(line:{line})',// 2018-10-27 19:01:21 [INFO] hello world test.php(line:8)
+        'errorTemplete' => '{date} [ERROR] {errstr} {errfile}(line:{errline}, errno:{errno})',// 2018-10-27 19:01:21 [ERROR] Undefined variable: var test.php(line:4, errno:8)
         'dateFormat' => 'Y-m-d H:i:s',
         'type' => 'file,print',//file,print,db
+        'info' => [
+            'function' => ['\icy2003\ihelpers\Logger', 'echo'],// string|callable
+        ],
         'file' => [
             'filePath' => '@icy2003/logs',
             'fileName' => function () {// string|callable
                 return date('Y-m-d') . '.log';
             },
             'flag' => FILE_APPEND // FILE_BINARY|FILE_APPEND
-        ],
-        'print' => [
-            'function' => ['\icy2003\ihelpers\Logger', 'echo'],// string|callable
         ],
     ],
     'Language' => [
