ab46cdbf8683e17e824dd36246a64c1a80fd05b1
icy2003 <2317216477@qq.com>
2018-10-27 22:40:42 +0800

    [*]修改 ERROR 模板，以区别 INFO 的

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'Logger' => [
         'isLog' => true,
         'infoTemplete' => '{date} [INFO] {file}(line:{line}) {message}',// 2018-10-27 19:01:21 [INFO] test.php(line:8) hello world
-        'errorTemplete' => '{date} [ERRO] {errfile}(line:{errline}) {errstr}{errno}',// 2018-10-27 19:01:21 [ERRO] test.php(line:4) Undefined variable: var(8)
+        'errorTemplete' => '{date} [ERROR:{errno}] {errfile}(line:{errline}) {errstr}',// 2018-10-27 19:01:21 [ERROR:8] test.php(line:4) Undefined variable: var
         'dateFormat' => 'Y-m-d H:i:s',
         'type' => 'file,print',//file,print,db
         'file' => [
