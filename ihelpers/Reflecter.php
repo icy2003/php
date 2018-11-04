b160e183128eb99f4c8f6ab153d8af4d39e3ca8d
icy2003 <2317216477@qq.com>
2018-11-04 22:52:39 +0800

    [+]添加 Reflecter.php 扩展反射类

---
+++ ihelpers/Reflecter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace icy2003\ihelpers;
+
+use ReflectionClass;
+
+class Reflecter
+{
+    private static $instance;
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+    /**
+     * @param mixed $object @see http://php.net/manual/zh/reflectionclass.construct.php
+     *
+     * @return static
+     */
+    public static function create()
+    {
+        if (!self::$instance instanceof self) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function load($object)
+    {
+        $this->reflecter = new ReflectionClass($object);
+        return $this;
+    }
+
+    /**
+     *
+     * @var \ReflectionClass $reflecter
+     */
+    private $reflecter;
+
+    /**
+     * @see http://php.net/manual/zh/class.reflectionclass.php
+     *
+     * @param string $name 反射类可调用的方法
+     * @param mixed $arguments 对应的参数
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        return $this->reflecter->$name($arguments);
+    }
+    /**
+     * 获取当前类的所有方法
+     *
+     * @param callback $callback 对方法名的回调，true 则返回该方法
+     * @return array
+     */
+    public function getCurrentClassMethods($callback = null)
+    {
+        $callback = null === $callback ? function ($name) {
+            return true;
+        } : $callback;
+        $currentClass = $this->reflecter->getName();
+        $methods = $this->reflecter->getMethods();
+        $result = [];
+        foreach ($methods as $method) {
+            if ($currentClass === $method->class && $callback($method->name)) {
+                $result[] = $method;
+            }
+        }
+        return $result;
+    }
+}
