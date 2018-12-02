0c384e030fee46eee7c0ec16561d3549218d2627
icy2003 <2317216477@qq.com>
2018-12-02 11:40:04 +0800

    [!]改成单例模式（原因是翻译工作在持久化应用里单例优势会很大！）

--- ihelpers/Language.php
+++ ihelpers/Language.php
@@ -6,12 +6,43 @@ use icy2003\BaseI;
 
 class Language
 {
-    public static function t($category, $message, $params = [], $language = 'zh-cn')
+    protected static $_instance;
+    private $__config;
+    private $__categories = [];
+
+    private function __construct()
     {
-        $config = BaseI::config('Language');
+    }
+
+    private function __clone()
+    {
+    }
+    /**
+     * 创建一个语言单例
+     *
+     * @return static
+     */
+    public static function create($config = [])
+    {
+        if (!static::$_instance instanceof static ) {
+            static::$_instance = new static();
+            static::$_instance->__config = BaseI::config('Language');
+        }
+
+        return static::$_instance;
+    }
+
+    public function t($category, $message, $params = [], $language = 'zh-cn')
+    {
+        $config = $this->__config;
         $language = !empty($language) ? $language : Arrays::value($config, 'language');
         $basePath = Arrays::value($config, 'basePath');
-        $map = require BaseI::getAlias(trim($basePath, '/') . "/{$language}/{$category}.php");
+        if (empty($this->__categories[$category . '_' . $language])) {
+            $map = include BaseI::getAlias(trim($basePath, '/') . "/{$language}/{$category}.php");
+            $this->__categories[$category . '_' . $language] = $map;
+        } else {
+            $map = $this->__categories[$category . '_' . $language];
+        }
         return str_replace(array_map(function ($data) {
             return '{' . $data . '}';
         }, array_keys($params)), array_values($params), Arrays::value($map, $message, $message));
