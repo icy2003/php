ccca06630e14b3447fcb58f04a6cb07c683426c3
icy2003 <2317216477@qq.com>
2019-01-18 14:49:42 +0800

    [*]把 Worksheet 改成 iWorksheet，加 i 前缀用于区别原类。路径改成 Phpspreadsheet 对应类的路径

--- iexts/phpspreadsheet/Worksheet/iWorksheet.php
+++ iexts/phpspreadsheet/Worksheet/iWorksheet.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace icy2003\iexts\phpspreadsheet;
+namespace icy2003\iexts\phpspreadsheet\Worksheet;
 
 use icy2003\ihelpers\Env;
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\RichText\RichText;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class Worksheet
+class iWorksheet
 {
     /**
      * 返回一个指定范围内的单元格的数组
@@ -20,14 +20,14 @@ class Worksheet
      * @param bool $calculateFormulas 是否计算公式的值？
      * @param bool $formatData 是否应用格式化到该数据
      * @param bool $returnCellRef false - 按照索引返回数组，true - 按照真实的行列返回数组
-     * @param bool $onlyVisible 是否只返回可见单元格
      * @param array $params 额外条件参数
+     *                  onlyVisible 是否只返回可见单元格，默认 true
      *                  fillColor 填充颜色，如："#ff0000"，表示只返回红色背景的数据
      *
      *
      * @return array
      */
-    public static function rangeToArray($workSheet, $pRange, $nullValue = null, $calculateFormulas = true, $formatData = true, $returnCellRef = false, $onlyVisible = false, $params = [])
+    public static function rangeToArray($workSheet, $pRange, $nullValue = null, $calculateFormulas = true, $formatData = true, $returnCellRef = false, $params = [])
     {
         $returnValue = [];
         list($rangeStart, $rangeEnd) = Coordinate::rangeBoundaries($pRange);
@@ -38,6 +38,8 @@ class Worksheet
 
         ++$maxCol;
         $r = -1;
+        $onlyVisible = Env::value($params, 'onlyVisible', true);
+        $fillColor = Env::value($params, 'fillColor', false);
         for ($row = $minRow; $row <= $maxRow; ++$row) {
             if (true === $onlyVisible) {
                 $rowVisible = $workSheet->getRowDimension($row)->getVisible();
@@ -55,12 +57,9 @@ class Worksheet
                     }
                 }
                 $cRef = ($returnCellRef) ? $col : ++$c;
-                if (!empty($params)) {
-                    $cellStyle = $workSheet->getStyle($col . $row);
-                    if (array_key_exists('fillColor', $params)) {
-                        if ($cellStyle->getFill()->getStartColor()->getARGB() != 'FF' . substr(strtoupper(Env::value($params, 'fillColor')), 1)) {
-                            continue;
-                        }
+                if ($fillColor) {
+                    if ($cellStyle->getFill()->getStartColor()->getARGB() != 'FF' . substr(strtoupper($fillColor), 1)) {
+                        continue;
                     }
                 }
                 if ($workSheet->getCellCollection()->has($col . $row)) {
@@ -98,45 +97,5 @@ class Worksheet
         }
 
         return $returnValue;
-    }
-
-    /**
-     * 返回一个指定范围内的可见的单元格的数组
-     *
-     * @param \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet $workSheet
-     * @param string $pRange 单元格范围 (i.e. "A1:B10"), 或者一个单元格 (i.e. "A1")
-     * @param bool $onlyVisible 是否只选择可见，true 是 false 否
-     * @param mixed $nullValue 单元格内容不存在时返回的值
-     * @param bool $calculateFormulas 是否计算公式的值？
-     * @param bool $formatData 是否应用格式化到该数据
-     * @param bool $returnCellRef false - 按照索引返回数组，true - 按照真实的行列返回数组
-     *
-     * @return array
-     */
-    public static function rangeToVisibleArray($workSheet, $pRange, $onlyVisible = true, $nullValue = null, $calculateFormulas = true, $formatData = true, $returnCellRef = false)
-    {
-        return static::rangeToArray($workSheet, $pRange, $nullValue, $calculateFormulas, $formatData, $returnCellRef, $onlyVisible);
-    }
-
-    /**
-     * 返回一个指定范围内的单元格的数组
-     *
-     * @param \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet $workSheet
-     * @param string $pRange 单元格范围 (i.e. "A1:B10"), 或者一个单元格 (i.e. "A1")
-     * @param array $params 额外条件参数
-     *                  onlyVisible 是否只返回可见单元格，默认“是”
-     *                  fillColor 填充颜色，如："#ff0000"，表示只返回红色背景的数据
-     * @param mixed $nullValue 单元格内容不存在时返回的值
-     * @param bool $calculateFormulas 是否计算公式的值？
-     * @param bool $formatData 是否应用格式化到该数据
-     * @param bool $returnCellRef false - 按照索引返回数组，true - 按照真实的行列返回数组
-     *
-     * @return array
-     */
-    public static function rangeToArrayByParams($workSheet, $pRange, $params, $nullValue = null, $calculateFormulas = true, $formatData = true, $returnCellRef = false)
-    {
-        $onlyVisible = Env::value($params, 'onlyVisible', true);
-        unset($params['onlyVisible']);
-        return static::rangeToArray($workSheet, $pRange, $nullValue, $calculateFormulas, $formatData, $returnCellRef, $onlyVisible, $params);
     }
 }
