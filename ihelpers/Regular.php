2fb9d945a9c9c59bd7a1b31fb6bee2ca60230185
icy2003 <2317216477@qq.com>
2019-02-21 20:59:20 +0800

    [+]添加手机号和身份证的正则

--- ihelpers/Regular.php
+++ ihelpers/Regular.php
@@ -21,8 +21,40 @@ class Regular
         return preg_match('/^[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/', $email);
     }
 
+    /**
+     * 验证 IP
+     * @see https://www.regular-expressions.info/ip.html
+     *
+     * @param string $ip
+     *
+     * @return boolean
+     */
     public static function ip($ip)
     {
         return preg_match('/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/', $ip);
+    }
+
+    /**
+     * 验证手机
+     *
+     * @param string $mobile
+     *
+     * @return boolean
+     */
+    public static function mobile($mobile)
+    {
+        return preg_match('/^1[3-9][0-9]\d{8}$/', $mobile);
+    }
+
+    /**
+     * 验证身份证号
+     *
+     * @param string $idCard
+     *
+     * @return boolean
+     */
+    public static function idCard($idCard)
+    {
+        return preg_match('/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$)/', $idCard);
     }
 }
