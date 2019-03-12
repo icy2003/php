9af4a973cb4db2c8bde1a4f696aba736b307309e
icy2003 <2317216477@qq.com>
2019-03-12 22:50:08 +0800

    [+]添加URL正则

--- src/ihelpers/Regular.php
+++ src/ihelpers/Regular.php
@@ -57,4 +57,16 @@ class Regular
     {
         return preg_match('/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$)/', $idCard);
     }
+
+    /**
+     * 验证 URL
+     *
+     * @param string $url
+     *
+     * @return boolean
+     */
+    public static function url($url)
+    {
+        return preg_match('/^https?:\/\//', $url);
+    }
 }
