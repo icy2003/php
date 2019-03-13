55b205483761ae7fbb7484a77663ee6e3b42e87a
icy2003 <2317216477@qq.com>
2019-03-13 11:06:03 +0800

    [*]给 Word 写入表格加入合并单元格的支持！！！！（现在还缺样式，不过已经可以起飞了）

--- src/iexts/phpword/TemplateProcessor.php
+++ src/iexts/phpword/TemplateProcessor.php
@@ -2,8 +2,10 @@
 
 namespace icy2003\php\iexts\phpword;
 
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpWord\IOFactory;
 use PhpOffice\PhpWord\PhpWord;
+use PhpOffice\PhpWord\SimpleType\Jc;
 use PhpOffice\PhpWord\TemplateProcessor as T;
 
 class TemplateProcessor extends T
@@ -104,13 +106,14 @@ class TemplateProcessor extends T
      *
      * @param string $var 变量名
      * @param array $array 二维数组
+     * @param array $mArray 合并单元格的数组，例如：["A1:B1", "C1:C2"]
      * @param boolean $useCellRef 是否使用单元格引用（例如A3），暂时没用
-     *                  格式参考 phpspreadsheet 的 rangeToArray 或者 icy2003/iexts/phpspreadsheet/Worksheet/iWorksheet rangeToArray
+     *                  格式参考 phpspreadsheet 的 rangeToArray
      * @todo 给表格加样式
      *
      * @return void
      */
-    public function setTable($var, $array, $useCellRef = true)
+    public function setTable($var, $array, $mArray = [], $useCellRef = true)
     {
         $phpWord = new PhpWord();
         $section = $phpWord->addSection();
@@ -121,12 +124,47 @@ class TemplateProcessor extends T
             'cellMarginRight' => 150,
         ]);
         $table = $section->addTable($style);
-        foreach ($array as $r => $row) {
-            // echo $r;
+        foreach ($array as $rowIndex => $row) {
+            // echo $rowIndex; 1
             $table->addRow();
+            $i = 0;
             foreach ($row as $c => $value) {
-                // echo $c;
-                $table->addCell()->addText($value);
+                if ($i > 0) {
+                    $i--;
+                    continue;
+                }
+                // echo $c; A
+                $colIndex = Coordinate::columnIndexFromString($c);
+                $array = [];
+                $isContinue = false;
+                foreach ($mArray as $range) {
+                    // 1,1 : 2,2
+                    list($rangeStart, $rangeEnd) = Coordinate::rangeBoundaries($range); // A1:B2
+                    if ($rangeEnd[0] > $rangeStart[0]) {
+                        if ($colIndex >= $rangeStart[0] && $colIndex <= $rangeEnd[0] && $rowIndex <= $rangeStart[1]) {
+                            $array = array_merge($array, ['gridSpan' => $rangeEnd[0] - $rangeStart[0] + 1]);
+                            $i = $rangeEnd[0] - $rangeStart[0];
+                        }
+                    }
+                    if ($rangeEnd[1] > $rangeStart[1] && $colIndex >= $rangeStart[0]) {
+                        if ($rowIndex == $rangeStart[1]) {
+                            $array = array_merge($array, ['vMerge' => 'restart']);
+                        } elseif ($rowIndex > $rangeStart[1] && $rowIndex <= $rangeEnd[1]) {
+                            $isContinue = true;
+                            $array = array_merge($array, ['vMerge' => 'continue']);
+                        }
+                    }
+                    if (!empty($array)) {
+                        break;
+                    }
+                }
+                $array = array_merge($array, ['valign' => Jc::CENTER]);
+                if (true === $isContinue) {
+                    $isContinue = false;
+                    $table->addCell(null, $array);
+                } else {
+                    $table->addCell(null, $array)->addText($value, null, ['alignment' => Jc::CENTER]);
+                }
             }
         }
         $objWriter = IOFactory::createWriter($phpWord);
