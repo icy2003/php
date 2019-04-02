643ff4d2dab0a0d59e845eccd085bb082140a03b
icy2003 <2317216477@qq.com>
2019-04-02 17:29:59 +0800

    [+]yii2的模型用于单元测试里的断言快捷方式

---
+++ src/iexts/UnitAssert.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace icy2003\php\iexts;
+
+/**
+ * yii2 模型的单元测试断言快捷方式
+ */
+class UnitAssert extends \Codeception\Test\Unit
+{
+    public static function true($model)
+    {
+        parent::assertTrue($model->validate(), implode($model->getFirstErrors()));
+    }
+
+    public static function false($model)
+    {
+        parent::assertFalse($model->validate(), implode($model->getFirstErrors()));
+    }
+}
