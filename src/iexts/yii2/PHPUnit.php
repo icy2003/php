38bdcbd6103b8f21820464d847f169c1173aaa18
icy2003 <2317216477@qq.com>
2019-04-02 19:35:13 +0800

    [fix]不能以test开头，别问我为什么 [*]针对某个属性进行校验，可以减少调用次数（当然因为是测试代码，无所谓）

--- src/iexts/yii2/PHPUnit.php
+++ src/iexts/yii2/PHPUnit.php
@@ -7,21 +7,21 @@ namespace icy2003\php\iexts\yii2;
  */
 class PHPUnit extends \Codeception\Test\Unit
 {
-    public static function true($model)
+    public static function true($model, $attributes = null)
     {
-        parent::assertTrue($model->validate(), implode($model->getFirstErrors()));
+        parent::assertTrue($model->validate($attributes), implode($model->getFirstErrors()));
     }
 
-    public static function false($model)
+    public static function false($model, $attributes = null)
     {
-        parent::assertFalse($model->validate(), implode($model->getFirstErrors()));
+        parent::assertFalse($model->validate($attributes), implode($model->getFirstErrors()));
     }
 
-    public static function testValue($model, $attribute, $array)
+    public static function checkAttribute($model, $attribute, $array)
     {
         if (2 === count($array) && isset($array[0]) && isset($array[1])) {
             $model->$attribute = $array[0];
-            static::false($model);
+            static::false($model, $attribute);
             $model->$attribute = $array[1];
         } else {
             throw new \Exception('数组 0 位元素表示错误的属性值，1 位元素表示正确的属性值');
