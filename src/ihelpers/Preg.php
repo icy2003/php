8881e7a57e72b4a2e1cacdc93d47350bc0813d87
icy2003 <2317216477@qq.com>
2019-04-22 17:27:00 +0800

    [~]更新注释

--- src/ihelpers/Preg.php
+++ src/ihelpers/Preg.php
@@ -1,7 +1,16 @@
 <?php
-
+/**
+ * Class Preg
+ *
+ * @link https://www.icy2003.com/
+ * @author icy2003 <2317216477@qq.com>
+ * @copyright Copyright (c) 2017, icy2003
+ */
 namespace icy2003\php\ihelpers;
 
+/**
+ * 正则
+ */
 class Preg
 {
     /**
