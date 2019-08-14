624de253de07605fe3a44c3c66442c7f25c58d2c
icy2003 <2317216477@qq.com>
2019-08-14 12:32:51 +0800

    [*]进制转换迁移

--- src/ihelpers/Numbers.php
+++ src/ihelpers/Numbers.php
@@ -8,6 +8,7 @@
  */
 namespace icy2003\php\ihelpers;
 
+use icy2003\php\C;
 use icy2003\php\I;
 
 /**
@@ -126,5 +127,127 @@ class Numbers
         $bytes1 = self::toBytes($size1);
         $bytes2 = self::toBytes($size2);
         return ($v = $bytes1 - $bytes2) == 0 ? 0 : intval(($v) / abs($v));
+    }
+
+    /**
+     * 将某进制的字符串转化成另一进制的字符串
+     *
+     * @see http://php.net/manual/zh/function.base-convert.php
+     *
+     * @param string $numberInput 待转换的字符串
+     * @param string $fromBaseInput 起始进制的规则
+     * @param string $toBaseInput 结束进制的规则
+     *
+     * @return string
+     */
+    public static function baseConvert($numberInput, $fromBaseInput, $toBaseInput)
+    {
+        if ($fromBaseInput == $toBaseInput) {
+            return $numberInput;
+        }
+
+        $fromBase = Strings::split($fromBaseInput);
+        $toBase = Strings::split($toBaseInput);
+        $number = Strings::split($numberInput);
+        $fromLen = Strings::length($fromBaseInput);
+        $toLen = Strings::length($toBaseInput);
+        $numberLen = Strings::length($numberInput);
+        $retval = '';
+        if ($toBaseInput == '0123456789') {
+            $retval = 0;
+            for ($i = 1; $i <= $numberLen; $i++) {
+                $retval = bcadd($retval, bcmul(array_search($number[$i - 1], $fromBase), bcpow($fromLen, $numberLen - $i)));
+            }
+
+            return $retval;
+        }
+        if ($fromBaseInput != '0123456789') {
+            $base10 = self::baseConvert($numberInput, $fromBaseInput, '0123456789');
+        } else {
+            $base10 = $numberInput;
+        }
+
+        if ($base10 < Strings::length($toBaseInput)) {
+            return $toBase[$base10];
+        }
+
+        while ($base10 != '0') {
+            $retval = $toBase[bcmod($base10, $toLen)] . $retval;
+            $base10 = bcdiv($base10, $toLen, 0);
+        }
+        return $retval;
+    }
+
+    /**
+     * 进制转换
+     *
+     * - 支持 2、8、10、16 进制之间转换
+     *
+     * @param string $number
+     * @param integer $fromBase 2、8、10、16之间互转
+     * @param integer $toBase 2、8、10、16之间互转
+     *
+     * @return string
+     */
+    public static function base($number, $fromBase, $toBase)
+    {
+        C::assertTrue(in_array($fromBase, [2, 8, 10, 16]), '原始进制必须为 2、8、10、16 之一');
+        C::assertTrue(in_array($toBase, [2, 8, 10, 16]), '目标进制必须为 2、8、10、16 之一');
+        $chars = '0123456789ABCDEF';
+        return self::baseConvert($number, Strings::sub($chars, 0, $fromBase), Strings::sub($chars, 0, $toBase));
+    }
+
+    /**
+     * 中文大写数字转成小写数字
+     *
+     * @param string $number
+     *
+     * @return string
+     */
+    public static function toLowerChineseCase($number)
+    {
+        return Strings::replace($number, [
+            '零' => '〇',
+            '壹' => '一',
+            '贰' => '二',
+            '叁' => '三',
+            '肆' => '四',
+            '伍' => '五',
+            '陆' => '六',
+            '柒' => '七',
+            '捌' => '八',
+            '玖' => '九',
+            '拾' => '十',
+            '佰' => '百',
+            '仟' => '千',
+            '圆' => '元',
+        ]);
+    }
+
+    /**
+     * 中文小写数字转成大写数字
+     *
+     * @param string $number
+     *
+     * @return string
+     */
+    public static function toUpperChineseCase($number)
+    {
+        return Strings::replace($number, [
+            '〇' => '零',
+            '一' => '壹',
+            '二' => '贰',
+            '三' => '叁',
+            '四' => '肆',
+            '五' => '伍',
+            '六' => '陆',
+            '七' => '柒',
+            '八' => '捌',
+            '九' => '玖',
+            '十' => '拾',
+            '百' => '佰',
+            '千' => '仟',
+            '元' => '圆',
+        ]);
     }
 }
